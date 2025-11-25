02d28c5cc47559a18a135413f3ca337502a56e4d
norat <norat@LAPTOP-3MDT35S1>
2025-11-25 16:39:56 +0100

    navegación tarjeta cumpleaños hecha

--- HelloWorld/app/src/main/java/noragarcia/helloworld/NavegacionActivity.kt
+++ HelloWorld/app/src/main/java/noragarcia/helloworld/NavegacionActivity.kt
@@ -1,35 +1,33 @@
 package noragarcia.helloworld
 
-
-
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import noragarcia.helloworld.databinding.ActivityMainBinding
+import noragarcia.helloworld.databinding.ActivityNavegacionBinding
+import android.content.Intent
 
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class NavegacionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNavegacionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityNavegacionBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-//setContentView(R.layout.activity_main)
-
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+       /* setContentView(R.layout.activity_navegacion)*/
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }*/
-        binding.textViewTitle.text = "Felicidades!"
-        binding.textViewSubtitle.text = "Disfruta del día"
-        binding.fotoTarta.setImageResource(R.drawable.tarta)
-    }
 
+        }
+        binding.botonFelicitacion.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
